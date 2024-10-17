760bb48d617d1a398a6d9bc98fd958afa589c0f7
sharayuaswale-23 <sharayuaswale123@gmail.com>
2024-10-17 17:39:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let springbtnEl = document.getElementById("springbtn");
+let summerbtn = document.getElementById("summerbtn");
+let autumnbtn = document.getElementById("autumnbtn");
+let winterbtn = document.getElementById("winterbtn");
+let imgsEl = document.getElementById("imgs");
+
+function showspring(){
+    imgsEl.src = "https://tse4.mm.bing.net/th?id=OIP.GVJSCuxHiFlElCNOaA-3gwHaFj&pid=Api&P=0&h=180";
+}
+function showsummer(){
+    imgsEl.src = "https://tse4.mm.bing.net/th?id=OIP.TJw9FojIpZzsWuqOBzdrtwHaEi&pid=Api&P=0&h=180";
+}
+function showautumn(){
+    imgsEl.src = "https://tse4.mm.bing.net/th?id=OIP.a0TXXJi07kZ7jnJU4Rr9xwHaEo&pid=Api&P=0&h=180";
+}
+function showwinter(){
+    imgsEl.src = "https://tse4.mm.bing.net/th?id=OIP.VAhsmRluQah-U5QKF5a4pQHaE7&pid=Api&P=0&h=180";
+}
